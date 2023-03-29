1373b348d8d53f750412a51287c0d84cb0d09ebd
Elvis <66243414+Elvis-1@users.noreply.github.com>
2023-03-29 17:30:08 +0100

    completed authentication for registration

--- lib/common/utils/constants.dart
+++ lib/common/utils/constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
   static const BASE_URL = 'https://examiner-cohort9-dev.up.railway.app';
   static const SIGN_UP = '/api/User/signup';
-  static const LOGIN = '';
+  static const LOGIN = '/api/User/login';
   static const RESET_PASSWORD = '';
 }
