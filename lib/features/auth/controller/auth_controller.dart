306c8522775a6fe574a479655d523a00b83be76b
Elvis <66243414+Elvis-1@users.noreply.github.com>
2023-03-31 13:54:29 +0100

    auth

--- lib/features/auth/controller/auth_controller.dart
+++ lib/features/auth/controller/auth_controller.dart
@@ -21,6 +21,33 @@ class AuthController extends GetxController {
     } catch (e) {
       responseModel = ResponseModel(message: e.toString(), isSuccess: false);
     }
+<<<<<<< Updated upstream
+=======
+    _isLoading = false;
+    update();
+    return responseModel;
+  }
+
+  Future<ResponseModel> signIn(body) async {
+    _isLoading = true;
+    update();
+    ResponseModel responseModel;
+    try {
+      Response response = await authRePository.signIn(body);
+      print(response.body);
+      if (response.statusCode == 201 && response.body['success'] == true) {
+        responseModel =
+            ResponseModel(message: 'Successfully Registered', isSuccess: true);
+      } else {
+        responseModel = ResponseModel(
+            message: response.body['resultMessage'], isSuccess: false);
+      }
+    } catch (e) {
+      responseModel = ResponseModel(message: 'failed', isSuccess: false);
+    }
+    _isLoading = false;
+    update();
+>>>>>>> Stashed changes
     return responseModel;
   }
 }
